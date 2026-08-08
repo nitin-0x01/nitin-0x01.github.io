bf5a1b03b18b8e8429de5fb09ab128ac47e99abb
Nitin Kumar Mandal <nitinkumarm28@gmail.com>
2026-08-08 12:24:20 +0530

    Enable sourcemap in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
@@ -10,5 +10,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, '.'),
     },
+  },
+  build: {
+    sourcemap: true,
   },
 });
